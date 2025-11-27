2be6826c616796b6a66811cec08e6971fe9306ac
hiuraaaaa <hiura0012@gmail.com>
2025-11-28 02:48:58 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { router } from '@/router';
-import { ChatProvider } from '@/context/ChatProvider';
-import '@/styles/theme.css';
-import '@/styles/globals.css';
+import { router } from './router';
+import { ChatProvider } from './context/ChatProvider';
+import './styles/theme.css';
+import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
