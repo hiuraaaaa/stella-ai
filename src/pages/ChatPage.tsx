3d359fb16faaf65a881890db5c7bc3fe06b6e985
hiuraaaaa <hiura0012@gmail.com>
2025-11-28 02:23:49 +0700

    Create ChatPage.tsx

---
+++ src/pages/ChatPage.tsx
@@ -0,0 +1,18 @@
+// src/pages/ChatPage.tsx
+import { useChatContext } from '@/context/ChatProvider';
+import { ChatHome } from '@/components/chat/ChatHome';
+import { ChatWindow } from '@/components/chat/ChatWindow';
+
+export function ChatPage() {
+  const { activeSession } = useChatContext();
+
+  if (!activeSession) {
+    return <ChatHome />;
+  }
+
+  return (
+    <div className="flex-1 flex">
+      <ChatWindow />
+    </div>
+  );
+}
