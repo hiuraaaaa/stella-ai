f3af72b1a76fc46b298ecaf1bd9fa0b423827bb5
hiuraaaaa <hiura0012@gmail.com>
2025-11-28 02:55:48 +0700

    Update RootLayout.tsx

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,15 +1,12 @@
-// src/layout/RootLayout.tsx
 import { Outlet } from 'react-router-dom';
 import { TopBar } from './TopBar';
 import { Sidebar } from './Sidebar';
 
 export function RootLayout() {
   return (
-    <div
-      className="min-h-screen flex flex-col bg-[var(--bg-main)]"
-      style={{ backgroundColor: 'var(--bg-main)' }}
-    >
+    <div className="min-h-screen flex flex-col bg-[var(--bg-main)]">
       <TopBar />
+
       <div className="flex flex-1">
         <Sidebar />
         <main className="flex-1 flex">
